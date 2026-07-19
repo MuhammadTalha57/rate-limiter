4ee220ff5ca323f6dcbf1c3e83c920a1fab26638
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-19 20:59:58 +0500

    Fixed formatting

--- tests/integration/check.route.test.ts
+++ tests/integration/check.route.test.ts
@@ -5,10 +5,10 @@ import app from "../../src/app.js";
 it("should rate limit requests according to tokens left", async () => {
 	const res1 = await request(app)
 		.post("/check")
-		.send({ key: "u1", config: { tokenBucket: {capacity: 1} } });
+		.send({ key: "u1", config: { tokenBucket: { capacity: 1 } } });
 	const res2 = await request(app)
 		.post("/check")
-		.send({ key: "u1", config: { tokenBucket: {capacit: 1} } });
+		.send({ key: "u1", config: { tokenBucket: { capacit: 1 } } });
 
 	expect(res1.body.allowed).toBe(true);
 	expect(res2.body.allowed).toBe(false);
