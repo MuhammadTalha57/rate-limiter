2f47859003e4f633ee4001116b5a142986bb1eec
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-19 23:14:36 +0500

    Added Redis

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import type { Request, Response } from "express";
 import express from "express";
 import "dotenv/config";
 import { defaultConfig } from "./config/defaultConfig.js";
+import { logger } from "./config/logger.js";
 import store from "./repositories/memoryStore.repository.js";
+import { redis } from "./repositories/redisStore.repository.js";
 import { checkRouter } from "./routes/check.route.js";
 import createTokenBucket from "./services/tokenBucket.service.js";
 
@@ -14,6 +16,26 @@ app.use(express.json());
 
 app.use("", checkRouter);
 
-app.get("/health", (_req: Request, res: Response) => res.json({ ok: true }));
+app.get("/health", async (_req: Request, res: Response) => {
+	let isHealthy = true;
+
+	// Check Redis Connection
+	try {
+		const pong = await redis.ping();
+		isHealthy = pong === "PONG";
+	} catch (e) {
+		isHealthy = false;
+		logger.error(`[Health] System is unhealthy and down: ${e}`);
+		res.status(503).json({ ok: isHealthy, redis: "DOWN" });
+	}
+
+	if (isHealthy) {
+		logger.info(`[Health] System is health and running.`);
+		res.status(200).json({ ok: isHealthy, redis: "UP" });
+	} else {
+		logger.error(`[Health] System is unhealthy and down.`);
+		res.status(503).json({ ok: isHealthy, redis: "DOWN" });
+	}
+});
 
 export default app;
