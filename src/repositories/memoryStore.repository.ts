9b4be0533069b521eea54387a126396e15c18002
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-20 21:33:30 +0500

    Redefined memory store

--- src/repositories/memoryStore.repository.ts
+++ src/repositories/memoryStore.repository.ts
@@ -1,5 +1,5 @@
 import { logger } from "../config/logger.js";
-import type { Bucket, Store } from "../types.js";
+import type { Bucket, Config, Store } from "../types.js";
 
 const inMemoryStore: Record<string, { tokens: number; lastUpdated: number }> =
 	{};
@@ -18,6 +18,42 @@ class MemoryStore implements Store {
 
 	async del(key: string) {
 		delete inMemoryStore[key];
+	}
+
+	async check(
+		key: string,
+		config: Config,
+	): Promise<{ allowed: boolean; remaining: number }> {
+		let result: { allowed: boolean; remaining: number };
+
+		const capacity = config.tokenBucket.capacity;
+		const refillRate = config.tokenBucket.refillRate;
+		let bucket = await this.get(key);
+
+		if (bucket === undefined) {
+			bucket = { tokens: capacity, lastUpdated: Date.now() };
+			await this.set(key, bucket);
+		}
+
+		const elapsedSeconds = Math.floor((Date.now() - bucket.lastUpdated) / 1000);
+		const newTokenCount = Math.min(
+			capacity,
+			bucket.tokens + refillRate * elapsedSeconds,
+		);
+		bucket.tokens = newTokenCount;
+		bucket.lastUpdated += elapsedSeconds * 1000;
+
+		if (bucket.tokens >= 1) {
+			// Consume
+			bucket.tokens--;
+			this.set(key, bucket);
+
+			result = { allowed: true, remaining: bucket.tokens };
+		} else {
+			result = { allowed: false, remaining: 0 };
+		}
+
+		return result;
 	}
 }
 
