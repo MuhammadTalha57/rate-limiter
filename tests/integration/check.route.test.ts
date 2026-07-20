18f8c28a83e201ddfe6a4a140a0d6fb5b00294ab
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-20 21:28:48 +0500

    Fixed integration test

--- tests/integration/check.route.test.ts
+++ tests/integration/check.route.test.ts
@@ -1,14 +1,23 @@
 import request from "supertest";
 import { expect, it } from "vitest";
 import app from "../../src/app.js";
+import store from "../../src/repositories/store.repository.js";
 
 it("should rate limit requests according to tokens left", async () => {
+	await store.del("u1");
+
 	const res1 = await request(app)
 		.post("/check")
-		.send({ key: "u1", config: { tokenBucket: { capacity: 1 } } });
+		.send({
+			key: "u1",
+			config: { tokenBucket: { capacity: 1, refillRate: 0 } },
+		});
 	const res2 = await request(app)
 		.post("/check")
-		.send({ key: "u1", config: { tokenBucket: { capacit: 1 } } });
+		.send({
+			key: "u1",
+			config: { tokenBucket: { capacity: 1, refillRate: 0 } },
+		});
 
 	expect(res1.body.allowed).toBe(true);
 	expect(res2.body.allowed).toBe(false);
