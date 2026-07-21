46f2a42243dfe94038865b8304ffe880a3033b39
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-21 22:49:11 +0500

    Refactored tests

--- tests/integration/check.route.test.ts
+++ tests/integration/check.route.test.ts
@@ -2,23 +2,28 @@ import request from "supertest";
 import { expect, it } from "vitest";
 import app from "../../src/app.js";
 import store from "../../src/repositories/store.repository.js";
+import type { Config } from "../../src/types.js";
 
 it("should rate limit requests according to tokens left", async () => {
 	await store.del("u1");
 
-	const res1 = await request(app)
-		.post("/check")
-		.send({
-			key: "u1",
-			config: { tokenBucket: { capacity: 1, refillRate: 0 } },
-		});
-	const res2 = await request(app)
-		.post("/check")
-		.send({
-			key: "u1",
-			config: { tokenBucket: { capacity: 1, refillRate: 0 } },
-		});
+	const overrides: Partial<Config> = {
+		algorithm: "tokenBucket",
+		tokenBucket: {
+			capacity: 1,
+			refillRate: 0,
+		},
+	};
+	const result1 = await request(app).post("/check").send({
+		key: "u1",
+		overrides,
+	});
 
-	expect(res1.body.allowed).toBe(true);
-	expect(res2.body.allowed).toBe(false);
+	const result2 = await request(app).post("/check").send({
+		key: "u1",
+		overrides,
+	});
+
+	expect(result1.body.allowed).toBe(true);
+	expect(result2.body.allowed).toBe(false);
 });
--- tests/unit/tokenBucket.test.ts
+++ tests/unit/tokenBucket.test.ts
@@ -1,50 +1,84 @@
 import { beforeEach, expect, it, vi } from "vitest";
+import { limiter } from "../../src/app.js";
 import store from "../../src/repositories/store.repository.js";
-import createTokenBucket from "../../src/services/tokenBucket.service.js";
+import type { Config } from "../../src/types.js";
 
 beforeEach(async () => {
 	await store.del("/api");
 });
 
 it("should allow request when tokens are available", async () => {
-	const bucket = createTokenBucket({ capacity: 100, refillRate: 100 }, store);
-
-	expect((await bucket.check("/api")).allowed).to.be.true;
+	const overrides: Partial<Config> = {
+		algorithm: "tokenBucket",
+		tokenBucket: {
+			capacity: 100,
+			refillRate: 100,
+		},
+	};
+	const result = await limiter.check("/api", overrides);
+	expect(result).to.be.true;
 });
 
 it("should not allow request when tokens are unavailable", async () => {
-	const bucket = createTokenBucket({ capacity: 1, refillRate: 0 }, store);
-
-	expect((await bucket.check("/api")).allowed).to.be.true;
-	expect((await bucket.check("/api")).allowed).to.be.false;
+	const overrides: Partial<Config> = {
+		algorithm: "tokenBucket",
+		tokenBucket: {
+			capacity: 1,
+			refillRate: 1,
+		},
+	};
+	const result1 = await limiter.check("/api", overrides);
+	expect(result1).to.be.true;
+	const result2 = await limiter.check("/api", overrides);
+	expect(result2).to.be.false;
 });
 
 it("should refill requests according to refillRate", async () => {
 	vi.useFakeTimers({ toFake: ["Date"] });
-	const bucket = createTokenBucket({ capacity: 1, refillRate: 1 }, store);
 
-	expect((await bucket.check("/api")).allowed).to.be.true;
-	expect((await bucket.check("/api")).allowed).to.be.false;
+	const overrides: Partial<Config> = {
+		algorithm: "tokenBucket",
+		tokenBucket: {
+			capacity: 1,
+			refillRate: 0.5,
+		},
+	};
 
-	// Wait For Refill
-	await vi.advanceTimersByTimeAsync(1000);
+	const result1 = await limiter.check("/api", overrides);
+	const result2 = await limiter.check("/api", overrides);
 
-	expect((await bucket.check("/api")).allowed).to.be.true;
+	await vi.advanceTimersByTimeAsync(2000);
+	const result3 = await limiter.check("/api", overrides);
+
+	expect(result1).to.be.true;
+	expect(result2).to.be.false;
+	expect(result3).to.be.true;
 
 	vi.useRealTimers();
 });
 
 it("should refill requests till capacity", async () => {
 	vi.useFakeTimers({ toFake: ["Date"] });
-	const bucket = createTokenBucket({ capacity: 2, refillRate: 1 }, store);
 
-	expect((await bucket.check("/api")).allowed).to.be.true;
-	expect((await bucket.check("/api")).allowed).to.be.true;
+	const overrides: Partial<Config> = {
+		algorithm: "tokenBucket",
+		tokenBucket: {
+			capacity: 1,
+			refillRate: 1,
+		},
+	};
 
-	// Wait For Refill [wait for 3s but capped at 2]
-	await vi.advanceTimersByTimeAsync(3000);
+	const result1 = await limiter.check("/api", overrides);
+	const result2 = await limiter.check("/api", overrides);
 
-	expect((await bucket.check("/api")).remaining).to.be.equal(1);
+	await vi.advanceTimersByTimeAsync(5000);
+	const result3 = await limiter.check("/api", overrides);
+	const result4 = await limiter.check("/api", overrides);
+
+	expect(result1).to.be.true;
+	expect(result2).to.be.false;
+	expect(result3).to.be.true;
+	expect(result4).to.be.false;
 
 	vi.useRealTimers();
 });
